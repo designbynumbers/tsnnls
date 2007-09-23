f586e06c0da6f37c6bf6fded5c1a181cfcbd665a
Jason Cantarella <jason.cantarella@gmail.com>
2007-09-23 19:30:48 +0000

    test_tsnnls working, make check working

---
+++ tsnnls/sp_test2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./tsnnls_test $srcdir/A_02.sparse $srcdir/b_02.mat $srcdir/x_02.mat --tsnnls
---
+++ tsnnls/sp_test4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./tsnnls_test $srcdir/A_04.sparse $srcdir/b_04.mat $srcdir/x_04.mat --tsnnls
