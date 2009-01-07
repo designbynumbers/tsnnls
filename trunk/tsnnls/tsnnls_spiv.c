e6b2f02f462302fb71446f2d8613943c2872b941
cantarel <cantarel@c12da72e-e447-0410-8b36-83a1620311f8>
2009-01-07 02:30:39 +0000

    Updated version number

--- trunk/tsnnls/tsnnls_spiv.c
+++ trunk/tsnnls/tsnnls_spiv.c
@@ -1,10 +1,10 @@
 
  
 /*
- * This program is free software distributed under the GPL. A copy of
+ * This program is free software distributed under the LGPL. A copy of
  * the license should have been included with this archive in a file
  * named 'LICENSE'. You can read the license there or on the web at:
- * http://www.gnu.org/licenses/gpl.txt
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
  */
 
 // This file is included at the end of tsnnls.c, so should be able to call
